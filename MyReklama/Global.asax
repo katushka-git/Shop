62d8f221506e8c9d395654ece912206136df41f5
katushka1936@gmail.com <katushka1936@gmail.com>
2020-01-27 12:55:09 +0200

    Добавьте файлы проекта.

---
+++ MyReklama/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyReklama.MvcApplication" Language="C#" %>
---
+++ MyReklama/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyReklama.Startup))]
+namespace MyReklama
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
