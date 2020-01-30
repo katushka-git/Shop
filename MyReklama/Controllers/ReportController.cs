262257e5b05283563a8307f6c79225037093b147
katushka1936@gmail.com <katushka1936@gmail.com>
2020-01-30 18:13:07 +0200

    30

---
+++ MyReklama/Controllers/ReportController.cs
@@ -0,0 +1,26 @@
+﻿using MyReklama.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyReklama.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+
+        private MyAppContext db = new MyAppContext();
+        public async Task<ActionResult> CountService() {
+            var result = db.Orders.GroupBy(item1 => item1.Service).Select(item => new { Name = item.Key.Name, Count = item.Count() }).ToList();
+            return View(result);
+        }
+        public ActionResult Index()
+        {
+            ViewBag.result = db.Orders.GroupBy(item1 => item1.Service).Select(item => new { Name = item.Key.Name, Count = item.Count() }).ToList();
+            return View();
+        }
+    }
+}
