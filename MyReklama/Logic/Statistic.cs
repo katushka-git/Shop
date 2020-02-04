1bd8950146e5d194d91c457697aba11c5e2d992d
katushka1936@gmail.com <katushka1936@gmail.com>
2020-02-04 10:28:15 +0200

    042

--- MyReklama/Logic/Statistic.cs
+++ MyReklama/Logic/Statistic.cs
@@ -5,8 +5,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+
 namespace MyReklama.Logic
 {
+   
     public class Statistic
     {
         ServiceView MyService;
--- MyReklama/Models/View/ServiceView.cs
+++ MyReklama/Models/View/ServiceView.cs
@@ -6,6 +6,7 @@ using System.Web;
 
 namespace MyReklama.Models.View
 {
+
     public class ServiceView
 
     {
