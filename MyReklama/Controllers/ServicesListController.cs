04052ca6d399e41ee0ce7508511fbd65043c7844
katushka1936@gmail.com <katushka1936@gmail.com>
2020-02-05 13:43:57 +0200

    0503

---
+++ MyReklama/Controllers/ServicesListController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyReklama.Models;
+
+namespace MyReklama.Controllers
+{
+    public class ServicesListController : Controller
+    {
+        private MyAppContext db = new MyAppContext();
+
+        // GET: ServicesList
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Services.ToListAsync());
+        }
+
+        // GET: ServicesList/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Service service = await db.Services.FindAsync(id);
+            if (service == null)
+            {
+                return HttpNotFound();
+            }
+            return View(service);
+        }
+
+        // GET: ServicesList/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ServicesList/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,Price")] Service service)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Services.Add(service);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(service);
+        }
+
+        // GET: ServicesList/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Service service = await db.Services.FindAsync(id);
+            if (service == null)
+            {
+                return HttpNotFound();
+            }
+            return View(service);
+        }
+
+        // POST: ServicesList/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,Price")] Service service)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(service).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(service);
+        }
+
+        // GET: ServicesList/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Service service = await db.Services.FindAsync(id);
+            if (service == null)
+            {
+                return HttpNotFound();
+            }
+            return View(service);
+        }
+
+        // POST: ServicesList/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Service service = await db.Services.FindAsync(id);
+            db.Services.Remove(service);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
